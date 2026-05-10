d7f4c82055187022cdc48db28cbdff40d9f4cc91
Jean Michel <jeanmichelms@gmail.com>
2026-05-10 16:35:34 -0300

    - update

--- AutoFlow/settings.py
+++ AutoFlow/settings.py
@@ -138,10 +138,10 @@ EMAIL_BACKEND = os.getenv(
     'EMAIL_BACKEND',
     'django.core.mail.backends.smtp.EmailBackend',
 )
-EMAIL_HOST = os.getenv('EMAIL_HOST', 'localhst') # Substitua pelo host SMTP real do serviço de e-mail que você usará (ex: 'smtp.gmail.com' para Gmail)
+EMAIL_HOST = os.getenv('EMAIL_HOST', 'localhost') # Substitua pelo host SMTP real do serviço de e-mail que você usará (ex: 'smtp.gmail.com' para Gmail)
 EMAIL_PORT = int(os.getenv('EMAIL_PORT', '587')) # Substitua pela porta real do serviço de e-mail (ex: 587 para Gmail com TLS, 465 para SSL)
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'xjeanmichel@gmail.com') # Substitua pelo e-mail real que enviará os avisos
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', 'kuyi nehk zdth ffrl') # Substitua pela senha real do e-mail (ou senha de app, se usar autenticação em duas etapas)
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'email@email.com') # Substitua pelo e-mail real que enviará os avisos
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', 'senha#senha') # Substitua pela senha real do e-mail (ou senha de app, se usar autenticação em duas etapas)
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True').lower() in (
     '1',
     'true',
